daf1992155b3ebce01d07a3146e84d513bc6ec1e
shadow.zhou <shadow.zhou@dji.com>
2018-08-30 18:07:59 +0800

    add test

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
-	"fmt"
+	"errors"
 )
 
-func main() {
-	fmt.Println("hello world 333")
+func Division(a, b float64) (float64, error) {
+	if b == 0 {
+		return 0, errors.New("除数不能为0")
+	}
+
+	return a / b, nil
 }
